82a5ab25471442b43f239c07426b3f26bfe6ae8a
Alina Karkhavaya <korhi1536@gmail.com>
2021-08-01 00:12:51 +0300

    add first UnitTest with moq

---
+++ TestCoreTask.Tests/CommentControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestCoreTask.BLL;
+using TestCoreTask.WEB.Controllers;
+using Xunit;
+
+namespace TestCoreTask.Tests
+{
+    public class CommentControllerTests
+    {
+        [Fact]
+        public async Task GetAllTestAsync()
+        {
+            // Arrange
+            var mock1 = new Mock<ICommentService>();
+            mock1.Setup(repo => repo.GetCommentsAsync()).Returns(Task.FromResult(GetTestComments()));
+            var mock2 = new Mock<IMapper>();
+            CommentController controller = new CommentController(mock1.Object, mock2.Object);
+
+            // Act
+            var result = await controller.GetAll();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        private IReadOnlyCollection<Comment> GetTestComments()
+        {
+            var comments = new List<Comment>
+            {
+                new Comment { Id=1, ParentId=0, Date=new DateTime(), Text="test", UserName="test"},
+                new Comment { Id=2, ParentId=0, Date=new DateTime(), Text="test", UserName="test"},
+                new Comment { Id=3, ParentId=0, Date=new DateTime(), Text="test", UserName="test"}
+            };
+            return comments;
+        }
+    }
+}
