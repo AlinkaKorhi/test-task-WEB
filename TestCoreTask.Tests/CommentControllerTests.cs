9a3176296060331c5223cb509e85b02da0e2ff4c
Alina Karkhavaya <korhi1536@gmail.com>
2021-08-01 19:36:38 +0300

    add unit tests for create new comment

--- TestCoreTask.Tests/CommentControllerTests.cs
+++ TestCoreTask.Tests/CommentControllerTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TestCoreTask.BLL;
 using TestCoreTask.WEB.Controllers;
+using TestCoreTask.WEB.Controllers.Models;
 using Xunit;
 
 namespace TestCoreTask.Tests
@@ -17,16 +18,53 @@ namespace TestCoreTask.Tests
         public async Task GetAllTestAsync()
         {
             // Arrange
-            var mock1 = new Mock<ICommentService>();
-            mock1.Setup(repo => repo.GetCommentsAsync()).Returns(Task.FromResult(GetTestComments()));
-            var mock2 = new Mock<IMapper>();
-            CommentController controller = new CommentController(mock1.Object, mock2.Object);
+            var mockService = new Mock<ICommentService>();
+            mockService.Setup(repo => repo.GetCommentsAsync()).Returns(Task.FromResult(GetTestComments()));
+            var mockMapper = new Mock<IMapper>();
+            CommentController controller = new CommentController(mockService.Object, mockMapper.Object);
 
             // Act
             var result = await controller.GetAll();
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateCommentTestAsync()
+        {
+            // Arrange
+            var mockComment = new Comment { ParentId = 0, Date = new DateTime(), Text = "test", UserName = "test" };
+            var mockCommentModel = new CommentModel { ParentId = 0, Date = new DateTime(), Text = "test", UserName = "test" };
+
+            var mockService = new Mock<ICommentService>();
+            mockService.Setup(repo => repo.CreateCommentAsync(mockComment)).Returns(Task.FromResult(CreateTestComment()));
+
+            var mockMapper = new Mock<IMapper>();
+
+            CommentController controller = new CommentController(mockService.Object, mockMapper.Object);
+
+            // Act
+            var result = await controller.Post(mockCommentModel);
+
+            // Assert
+            Assert.IsType<ActionResult<CommentModel>>(result);
+        }
+
+        [Fact]
+        public async Task CreateCommentBadTestAsync()
+        {
+            // Arrange
+            var mockService = new Mock<ICommentService>();
+            var mockMapper = new Mock<IMapper>();
+
+            CommentController controller = new CommentController(mockService.Object, mockMapper.Object);
+
+            // Act
+            var result = await controller.Post(null);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result.Result);
         }
 
         private IReadOnlyCollection<Comment> GetTestComments()
@@ -39,5 +77,13 @@ namespace TestCoreTask.Tests
             };
             return comments;
         }
+
+        private Comment CreateTestComment()
+        {
+            var comment = new Comment { Id = 1, ParentId = 0, Date = new DateTime(), Text = "test", UserName = "test" };
+            return comment;
+        }
+
+
     }
 }
